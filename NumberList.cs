cf9ecb0430b7a9cab71cec8c8f98e68da96b81d6
AThomasBring <73478932+AThomasBring@users.noreply.github.com>
2022-06-14 10:37:36 +0100

    Added Migration, context, connection string, methods for the menu, calculation of time, all left now is the convert to Json bit

---
+++ NumberList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace NumberApp
+{
+    public class Context : DbContext
+    {
+        public DbSet<NumberList> NumberList { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=(local)\SQLExpress;Initial Catalog=NumbersLists;Integrated Security=SSPI;");
+        }
+    }
+    public class NumberList
+    {
+        public int Id { get; set; }
+        public string NumList { get; set; }
+        public int MilliSecondsTaken { get; set; }
+        public string SortDirection { get; set; }
+
+
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,82 +1,143 @@
-﻿using System.Diagnostics;
+﻿using Newtonsoft.Json;
+using NumberApp;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 
-Console.WriteLine("Please input any int and press enter inbetween each number.");
-Console.WriteLine("When the list is done press enter with no input to get options");
+SqlConnection connection = new SqlConnection(@"Data Source=(local)\SQLExpress;Initial Catalog=NumbersLists;Integrated Security=SSPI;");
 
 List<int> numbers = new List<int>();
 var timeToSortAsc = 0;
 var timeToSortDesc = 0;
 
+ReadNumbers();
 
-while (true)
+void ReadNumbers()
 {
-    var input = Console.ReadLine();
-    int value;
-    if(input == null || input == "")
+    Console.Clear();
+    Console.WriteLine("Please input any int and press enter inbetween each number.");
+    Console.WriteLine("When the list is done press enter with no input to get options");
+
+    while (true)
     {
-        break;
+        var input = Console.ReadLine();
+        int value;
+        if (input == null || input == "")
+        {
+            break;
+        }
+        else if (!string.IsNullOrEmpty(input) && int.TryParse(input, out value))
+        {
+            numbers.Add(value);
+        }
+        else
+        {
+            Console.WriteLine("That was not a valid number please try again");
+        }
     }
-    else if(!string.IsNullOrEmpty(input) && int.TryParse(input, out value))
+    OrderTheList();
+}
+
+void OrderTheList()
+{
+    var stopwatch = new Stopwatch();
+
+    stopwatch.Start();
+    var ascList = numbers.OrderBy(x => x).ToList();
+    stopwatch.Stop();
+    timeToSortAsc = Convert.ToInt32(stopwatch.ElapsedMilliseconds);
+
+    stopwatch.Start();
+    var descList = numbers.OrderByDescending(x => x).ToList();
+    stopwatch.Stop();
+    timeToSortDesc = Convert.ToInt32(stopwatch.ElapsedMilliseconds);
+
+    Console.WriteLine("Choose an ordering option from the following list:");
+    Console.WriteLine("\t1 - Ascending");
+    Console.WriteLine("\t2 - Descending");
+
+    switch (Console.ReadLine())
     {
-        numbers.Add(value);
-    }    
-    else
-    {
-        Console.WriteLine("That was not a valid number please try again");
+        case "1":
+            Console.Clear();
+            Console.WriteLine("Here is your list in ascending order: ");
+            ascList.ForEach(num => { Console.Write(num + " "); });
+            Console.WriteLine();
+            Console.WriteLine("It took: " + timeToSortAsc + " milliSeconds");
+            SaveToDatabase(ascList, timeToSortAsc, "Ascending");
+            break;
+        case "2":
+            Console.Clear();
+            Console.WriteLine("Here is your list in descending order: ");
+            descList.ForEach(num => { Console.Write(num + " "); });
+            Console.WriteLine();
+            Console.WriteLine("It took: " + timeToSortDesc + " milliSeconds");
+            SaveToDatabase(descList, timeToSortDesc, "Descending");
+            break;
     }
-
+    Menu();
 }
-var stopwatch = new Stopwatch();
 
-stopwatch.Start();
-var ascList = numbers.OrderBy(x => x).ToList();
-stopwatch.Stop();
-timeToSortAsc = Convert.ToInt32(stopwatch.ElapsedMilliseconds);
 
-stopwatch.Start();
-var descList = numbers.OrderByDescending(x => x).ToList();
-stopwatch.Stop();
-timeToSortDesc = Convert.ToInt32(stopwatch.ElapsedMilliseconds);
 
-Console.WriteLine("Choose an ordering option from the following list:");
-Console.WriteLine("\t1 - Ascending");
-Console.WriteLine("\t2 - Descending");
-
-switch (Console.ReadLine())
+void Menu()
 {
-    case "1":
-        Console.WriteLine("Here is your list in ascending order: ");
-        ascList.ForEach(num => { Console.Write(num + " "); });
-        Console.WriteLine();
-        Console.WriteLine("It took: " + timeToSortAsc + " milliSeconds");
-        break;
-    case "2":
-        Console.WriteLine("Here is your list in descending order: ");
-        descList.ForEach(num => { Console.Write(num + " "); });
-        Console.WriteLine();
-        Console.WriteLine("It took: " + timeToSortDesc + " milliSeconds");
-        break;
+    Console.WriteLine();
+    Console.WriteLine("Choose what to do next:");
+    Console.WriteLine("\t1 - Convert Lists to JSON");
+    Console.WriteLine("\t2 - Add another list");
+    Console.WriteLine("\t3 - Exit");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            ConvertToJson();
+            break;
+        case "2":
+            ReadNumbers();
+            break;
+        case "3":
+            Console.WriteLine("Thank you have a great day!");
+            break;
+
+    }
 }
 
-//Save to database
 
 
-Console.WriteLine("Choose what to do next:");
-Console.WriteLine("\t1 - Convert Lists to JSON");
-Console.WriteLine("\t2 - Exit");
 
-switch (Console.ReadLine())
+
+void ConvertToJson()
 {
-    case "1":
-        Console.WriteLine("Here is your list in ascending order: ");
-        ascList.ForEach(num => { Console.Write(num + " "); });
-        break;
-    case "2":
-        Console.WriteLine("Here is your list in descending order: ");
-        descList.ForEach(num => { Console.Write(num + " "); });
-        break;
-
+    var context = new Context();
+    var listOfLists = context.NumberList.ToList();
+    var jsonList = new List<string>();
+    foreach (var listItem in listOfLists)
+    {
+        string jsonItem = JsonConvert.SerializeObject(listItem, Formatting.Indented);
+        jsonList.Add(jsonItem);
+    }
+    Console.WriteLine();
+    Console.WriteLine(jsonList);
+    Menu();
 }
+
+void SaveToDatabase(List<int> list, int timeToSort, string direction)
+{
+    List<string> strings = list.ConvertAll<string>(x => x.ToString());
+    var stringList = String.Join(", ", strings);
+
+    NumberList numList = new NumberList()
+    {
+        NumList = stringList,
+        MilliSecondsTaken = timeToSort,
+        SortDirection = direction
+    };
+
+    var context = new Context();
+    context.NumberList.Add(numList);
+    context.SaveChanges();
+}
+
 
 
